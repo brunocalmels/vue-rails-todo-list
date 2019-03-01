0f142d9b95ce36d4d5f8020245df1ad2dfa33bbb
Bruno Calmels <bruno.calmels@macherit.com>
2019-03-01 13:28:26 -0300

    feat[manifest]: Agrega manifest.json e imagenes de iconos

--- config/initializers/service_worker.rb
+++ config/initializers/service_worker.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   config.serviceworker.routes.draw do
     # map to assets implicitly
     match '/serviceworker.js', pack: 'serviceworker.js'
+    match "/manifest.json"
 
     # Examples
     #
